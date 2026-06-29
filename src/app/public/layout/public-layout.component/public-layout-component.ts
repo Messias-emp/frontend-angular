5118ab3cbcde313fecf2b1c7be86f458af303c02
Messias-emp <empinheiro3.0@gmail.com>
2026-06-28 22:03:23 -0300

    atualizaçao front

--- src/app/public/layout/public-layout.component/public-layout-component.ts
+++ src/app/public/layout/public-layout.component/public-layout-component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import {  RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
-import { FooterComponent }
-  from '../../../shared/components/navbar/footer.component';
-import { Home } from '../../../shared/components/home/home';
+import { FooterComponent }from '../../../shared/components/navbar/footer.component';
+
 
 
 
