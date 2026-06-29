291d785e86a692fd1f7b2a2934e6b425bd023949
Messias-emp <empinheiro3.0@gmail.com>
2026-06-28 21:52:35 -0300

    atualizaçao front

--- src/app/public/layout/public-layout.component/public-layout-component.ts
+++ src/app/public/layout/public-layout.component/public-layout-component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {  RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { FooterComponent }
   from '../../../shared/components/navbar/footer.component';
@@ -12,7 +12,7 @@ import { Home } from '../../../shared/components/home/home';
 @Component({
   standalone: true,
   selector: 'PublicLayoutComponent',
-  imports: [ NavbarComponent, FooterComponent, Home],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './public-layout-component.html',
   styleUrl: './public-layout-component.scss',
 
